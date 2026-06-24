6acf1b195118ad9951e6cf9b2b5d3bf122adb3e6
Jatin Kumar <anshkhoba@gmail.com>
2026-06-24 09:16:13 +0530

    Fix logic for ten multiples problem

---
+++ Miscellaneous/TenMultiplesStop.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class TenMultiplesStop {
+    public static void checkMultiples() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a valid number");
+        int n = sc.nextInt();
+        while (n > 0) {
+            if (n % 10 == 0) {
+                System.out.println("You entered a valid number congrats !");
+                break;
+            } else {
+                System.out.println("Enter a valid number again");
+                n = sc.nextInt();
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        checkMultiples();
+    }
+}
