272b3462cf9c647d92e6c2994a8db39f7f26062a
Jatin Kumar <anshkhoba@gmail.com>
2026-06-28 16:10:19 +0530

    Print all primes in a range

---
+++ Miscellaneous/PrintPrime.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class PrintPrime {
+    public static boolean isPrime(int n) {
+        if (n == 2) {
+            return true;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void print_prime_num(int n) {
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i) == true) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        print_prime_num(n);
+    }
+}
