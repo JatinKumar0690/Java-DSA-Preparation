8503638608f37d22c2420f384f078ef190044249
Jatin Kumar <anshkhoba@gmail.com>
2026-06-30 16:39:17 +0530

    Printed diamond

---
+++ Patterns_Problems/Diamond.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Diamond {
+    public static void diamond_pattern(int n) {
+        // First Half
+        for (int i = 1; i <= n; i++) {
+            // spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // stars
+            for (int j = 1; j <= (2 * i) - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        // second Half
+        for (int i = n; i >= 1; i--) {
+            // spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // stars
+            for (int j = 1; j <= (2 * i) - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        diamond_pattern(n);
+        sc.close();
+    }
+}
