48b4ea102fd29b74bfcdda913d879138e0f7f66b
Jatin Kumar <anshkhoba@gmail.com>
2026-07-14 09:32:09 +0530

    Implemented counting sort algorithm

---
+++ Sorting/CountingSort.java
@@ -0,0 +1,29 @@
+public class CountingSort {
+    public static void countingSort(int nums[]) {
+        int largest = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            largest = Math.max(largest, nums[i]);
+        }
+        int count[] = new int[largest + 1];
+        for (int i = 0; i < nums.length; i++) {
+            count[nums[i]]++;
+        }
+        // sorting
+        int j = 0;
+        for (int i = 0; i < count.length; i++) {
+            while (count[i] > 0) {
+                nums[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        int nums[] = { 2, 3, 1, 4, 1 };
+        countingSort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
